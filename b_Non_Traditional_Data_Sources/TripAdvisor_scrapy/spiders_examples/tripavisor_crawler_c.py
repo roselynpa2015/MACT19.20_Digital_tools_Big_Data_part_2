63ec0dbff73a2b946edfa4e7c289b5b48064a6e0
diego <diegopajarito@gmail.com>
2020-02-22 13:13:59 +0100

    Updated spiders to follow loops and restaurant details.

--- b_Non_Traditional_Data_Sources/TripAdvisor_scrapy/spiders_examples/tripavisor_crawler_c.py
+++ b_Non_Traditional_Data_Sources/TripAdvisor_scrapy/spiders_examples/tripavisor_crawler_c.py
@@ -1,3 +1,25 @@
+# encoding: utf-8
+
+##################################################
+# This script corresponds to a "spider" component for the scrapy platform and
+# allows to extract different components from the tripadvisor web platform
+# c. The spider calls out to attributes and join them into a single text variable.
+# Documentation about scrapy and the tutorial used to develop this script
+# visit: https://docs.scrapy.org/en/latest/intro/tutorial.html
+##################################################
+#
+##################################################
+# Author: Iacopo Testi and Diego Pajarito
+# Copyright: Copyright 2020, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.3.0
+# Maintainer: Diego Pajarito
+# Email: diego.pajarito@iaac.net
+# Status: development
+##################################################
+
+
 import scrapy
 
 
@@ -9,8 +31,10 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for restaurant in response.css("a._15_ydu6b"):
+            # There are options for minimal text formatting tools
             yield {
                 'name': restaurant.css("a._15_ydu6b::text").get(),
                 'name_array': restaurant.css("a._15_ydu6b::text").getall(),
+                'name_joined': ' '.join(restaurant.css("a._15_ydu6b::text").getall()),
                 'url': restaurant.attrib['href']
             }
--- b_Non_Traditional_Data_Sources/TripAdvisor_scrapy/spiders_examples/tripavisor_crawler_e.py
+++ b_Non_Traditional_Data_Sources/TripAdvisor_scrapy/spiders_examples/tripavisor_crawler_e.py
@@ -3,7 +3,7 @@
 ##################################################
 # This script corresponds to a "spider" component for the scrapy platform and
 # allows to extract different components from the tripadvisor web platform
-# d. The spider uses the restaurant's url to call out a new page containing more details.
+# e. The spider calls out finds the next button to recursively navigate the full list of restaurants.
 # Documentation about scrapy and the tutorial used to develop this script
 # visit: https://docs.scrapy.org/en/latest/intro/tutorial.html
 ##################################################
@@ -24,28 +24,29 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "tripadvisor_d"
+    name = "tripadvisor_e"
     start_urls = [
         'https://www.tripadvisor.com/Restaurants-g294074-Bogota.html',
     ]
 
     def parse(self, response):
         for restaurant in response.css("a._15_ydu6b"):
-            yield {
-                'name_joined': ' '.join(restaurant.css("a._15_ydu6b::text").getall()),
-                'url': restaurant.attrib['href']
-            }
 
             # Extract the URL address (relative) to call a different parse method
             restaurant_url = response.urljoin(restaurant.attrib['href'])
             yield scrapy.Request(restaurant_url, callback=self.parse_restaurant)
 
+        # Find the URL address for the next page
+        next_page = response.xpath("//div[@class='unified pagination js_pageLinks']/a[@class='nav next rndBtn ui_button primary taLnk']").attrib['href']
+        if next_page is not None:
+            # Build an absolute path for the next page and call out the 'parse' method recursively
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
     # This method searches the restaurant details
     def parse_restaurant(self, response):
 
-        # You need to find new classes to extract data from this page
         yield {
-
             'title': response.xpath("//h1[@class='ui_header h1']/text()").get(),
             'icon_prize': response.xpath(
                 "//div[@class='header_links']/a[@href='/Restaurants-g294074-Bogota.html?pid=6']/text()").get(),
@@ -56,5 +57,4 @@ class QuotesSpider(scrapy.Spider):
             'number_reviews': response.xpath(
                 "//div[@class='restaurants-detail-overview-cards-RatingsOverviewCard__primaryRatingRow--VhEsu']/a[@href='#REVIEWS']/text()").get(),
             'street': response.xpath("//span[@class='street-address']/text()").get()
-
         }
